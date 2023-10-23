2abbafe8cc01953417013bf28d1775605a8445c5
Infinity <85234537+guptaanant682@users.noreply.github.com>
2023-10-23 12:42:39 +0530

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void duplicate(int arr[], int size){
+    int a;
+        
+
+}
+int main(){
+int arr[6]= {3,4,6,3,5,1};
+printarray(arr, 6);
+return 0;
+}
